78b0f02b99a2268d324570ae6b5e5b4a4c6163b0
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2015-02-16 16:48:17 +0300

    Factorial

---
+++ Semester2/1/task1/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Input the number");
+            long number = System.Convert.ToInt64(System.Console.ReadLine());
+            System.Console.WriteLine(Factorial(number));
+        }
+
+        static long Factorial(long number)
+        {
+            if (number == 0)
+                return 1;
+            return number * Factorial(number - 1);
+        }
+    }
+}

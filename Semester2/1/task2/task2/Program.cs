0138ec947cb897509d772e97bfa96d6dc4ee8c01
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2015-02-16 16:48:59 +0300

    Fibonachi

---
+++ Semester2/1/task2/task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Input the index");
+            int index = System.Convert.ToInt32(System.Console.ReadLine());
+            System.Console.WriteLine(Fibonachi(index));
+        }
+
+          static int Fibonachi(int index)
+        {
+            if (index == 0 || index == 1)
+                return 1;
+            else
+            {
+                int a = 1;
+                int b = 1;
+                int result = 0;
+                for (int i = 2; i <= index; i++)
+                {
+                    result = a + b;
+                    a = b;
+                    b = result;
+                }
+                return result;
+            }
+        }
+    }
+}

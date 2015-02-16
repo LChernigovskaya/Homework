0a2839e150c3841271c5f2538bcc29ec08caad15
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2015-02-16 21:05:40 +0300

    spiral

---
+++ Semester2/1/task6/task6/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the size of array");
+            int size = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the array of numbers");
+            int[,] numbers = new int[size, size];
+            for (int i = 0; i < size; i++)
+            {
+                string enterString = Console.ReadLine();
+                string[] massiveString = enterString.Split(new Char[] { ' ' });
+                for (int j = 0; j < massiveString.Length; j++)
+                {
+                    numbers[i, j] = int.Parse(massiveString[j]);
+                }
+            }
+            RoundOnASpiral(numbers, size);
+        }
+
+        static void RoundOnASpiral(int[,] numbers, int size)
+        {
+            int i = size / 2;
+            int j = size / 2;
+            Console.Write(numbers[i, j] + " ");
+
+            // one element matrix
+            if (i == 0 && j == 0)
+                return;
+            
+            //other cases
+            Console.Write(numbers[i - 1, j] + " ");
+            i--;
+
+
+            for (int k = 1; k <= size / 2 ; k++)
+            {
+                while (j < size / 2 + k)
+                {
+                    j++;
+                    Console.Write(numbers[i, j] + " ");
+                }
+
+                while (i < size / 2 + k)
+                {
+                    i++;
+                    Console.Write(numbers[i, j] + " ");
+                }
+
+                while (j > size / 2 - k)
+                {
+                    j--;
+                    Console.Write(numbers[i, j] + " ");
+                }
+
+                while (i > size / 2 - k)
+                {
+                    i--;
+                    Console.Write(numbers[i, j] + " ");
+                }
+            }
+        }
+    }
+}

5cc940443c45a8faba08d2ccea371c7c8c8a6d97
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2015-02-23 17:56:29 +0300

    Реализация стека в с#

---
+++ Semester2/2/task1/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            Console.WriteLine(stack.IsEmpty());
+            stack.Push(5);
+            stack.Push(1);
+            stack.Push(15);
+            stack.PrintStack();
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.IsEmpty());
+            stack.Push(55);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.IsEmpty());
+            stack.PrintStack();
+        }
+    }
+}
---
+++ Semester2/2/task1/task1/Stack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Task1
+{
+    class Stack
+    {
+
+        private StackElement head;
+
+        public void Push(int value)
+        {
+            StackElement newElement = new StackElement(value, head);
+            head = newElement;
+        }
+
+        public int Pop()
+        {
+            int value = head.value;
+            head = head.next;
+            return value;
+        }
+
+        public bool IsEmpty()
+        {
+            return (head == null);
+        }
+
+        public void PrintStack()
+        {
+            StackElement auxilary = head;
+            while (auxilary != null)
+            {
+                Console.Write(auxilary.value + " ");
+                auxilary = auxilary.next;
+            }
+            Console.WriteLine();
+        }
+
+        private class StackElement
+        {
+            public StackElement(int value, StackElement next)
+            {
+                this.value = value;
+                this.next = next;
+            }
+
+            public int value;
+            public StackElement next;
+        }
+
+    }
+    
+}

e1c8cf52e0e1562a013502bb6c633e8eb8e59d51
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2015-03-17 20:15:32 +0300

    Дерево разбора

---
+++ Semester2/4/task1/task1/Value.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task1
+{
+    /// <summary>
+    /// Operand
+    /// </summary>
+    public class Value : TreeElement
+    {
+        public Value(string value)        
+            : base(value, null, null)
+        { }
+        
+        public override double Calculate()
+        {
+            return Convert.ToDouble(Value);
+        }
+
+        /// <summary>
+        /// Print operand
+        /// </summary>
+        public override void Print()
+        {
+            Console.Write(Value);
+        }
+    }
+}

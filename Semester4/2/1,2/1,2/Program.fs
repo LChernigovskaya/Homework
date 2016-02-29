5a438434fa0146430dc3877df63bd5d252c9011b
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2016-02-29 19:04:16 +0300

    task 1,2

---
+++ Semester4/2/1,2/1,2/Program.fs
@@ -0,0 +1,10 @@
+﻿let rec figerMult number =
+    if abs(number) < 10 then number
+    else number % 10 * figerMult (number / 10)
+
+let findInd ls number = 
+    let rec findIndRec ls number index =
+        match ls with
+        | h :: t -> if h = number then index else findIndRec t number (index + 1)
+        | [] -> -1
+    findIndRec ls number 0

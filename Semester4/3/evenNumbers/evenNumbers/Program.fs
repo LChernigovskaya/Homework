e58481e22017f377c1b1603c08a9961fe9359e0f
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2016-03-13 17:23:12 +0300

    quantity of even numbers in a list

---
+++ Semester4/3/evenNumbers/evenNumbers/Program.fs
@@ -0,0 +1,8 @@
+﻿let evenNumbersFilter ls =
+    (List.filter(fun x -> x % 2 = 0) ls).Length
+
+let evenNumbersFold ls =
+    List.fold(fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0 ls
+
+let evenNumbersMap ls =
+    List.sum (List.map(fun x -> (x + 1) % 2) ls)

5dce65502afcc2285a5a8778a64d805f7414eb8f
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2016-03-14 12:09:59 +0300

    infinite sequence of prime numbers

---
+++ Semester4/3/seqOfPrimeNumbers/seqOfPrimeNumbers/Program.fs
@@ -0,0 +1,9 @@
+﻿let isPrime n =
+    let maxDiv = sqrt (float n)
+    let rec check i =
+        i > (int maxDiv) || (n % i <> 0 && check (i + 1))
+    check 2
+
+let infSeqOfPrime =
+    Seq.initInfinite (fun index -> if index = 0 then 2 else 2 * index + 1) |> 
+                                   Seq.filter(fun x -> isPrime x)

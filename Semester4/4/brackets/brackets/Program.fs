3c9dc3f8db42fc615010804244a0c6c0cec26276
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2016-03-14 15:08:34 +0300

    the correctness of sequence of brackets

---
+++ Semester4/4/brackets/brackets/Program.fs
@@ -0,0 +1,15 @@
+﻿let isCorrect (str: string) =
+    let list = List.ofArray (str.ToCharArray())
+    let rec closingBracket char st =
+        if List.isEmpty st then false 
+        else List.head st = char
+    let rec helper ls stack =
+        match ls with
+        | [] -> List.isEmpty stack
+        | h :: t -> match h with
+                    | _ when h = '(' || h = '{' || h = '[' -> helper t (h :: stack)
+                    | ')' -> if closingBracket '(' stack then helper t stack.Tail else false
+                    | '}' -> if closingBracket '{' stack then helper t stack.Tail else false
+                    | ']' -> if closingBracket '[' stack then helper t stack.Tail else false
+                    | _ -> helper t stack
+    helper list []

b3c0858dfcf82489eb8857e2066fae24f9083b4e
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2016-04-01 12:57:20 +0300

    stack

---
+++ Semester4/controlWork/polinom/polinom/Program.fs
@@ -0,0 +1,10 @@
+﻿type Polinom =
+    | Constant of string
+    | Variable of string
+    | Addition of Polinom * Polinom
+    | Subtraction of Polinom * Polinom
+    | Multiplication of Polinom * Polinom
+
+let degree n (pol: Polinom) =
+    match pol with
+    | Variable(x) -> x 

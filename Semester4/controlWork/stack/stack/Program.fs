27b779b314a92d1b80c711de07ebaad604c58f81
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2016-04-01 13:35:00 +0300

    stack

--- Semester4/controlWork/stack/stack/Program.fs
+++ Semester4/controlWork/stack/stack/Program.fs
@@ -1,7 +1,33 @@
-﻿// Дополнительные сведения о F# см. на http://fsharp.org
-// Дополнительную справку см. в проекте "Учебник по F#".
+﻿open System
+open System.Collections.Generic
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // возвращение целочисленного кода выхода
+exception ErrorEmpty of string
+
+type Stack<'a>() = 
+   let list = new List<'a>()
+   
+   member this.push value = 
+      list.Add(value)
+
+   member this.pop() =
+      if (this.IsEmpty()) then
+         raise(ErrorEmpty("empty stack")) 
+      let lastInd = list.Count - 1
+      let value = list.Item (lastInd)
+      list.RemoveAt (lastInd)
+      value
+
+   member this.IsEmpty() =
+      list.Count = 0
+
+let stack = new Stack<int>()
+stack.push 3
+stack.push 2
+
+try
+   printfn "%d" (stack.pop())
+   printfn "%d" (stack.pop())
+   printfn "%d" (stack.pop())
+    
+with
+    | ErrorEmpty(message) -> printfn "%A" message

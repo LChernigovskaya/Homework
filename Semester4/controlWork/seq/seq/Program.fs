6ba04f512fac6bfd284299c4bf396b1b99292c70
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2016-04-01 12:06:55 +0300

    task1

---
+++ Semester4/controlWork/seq/seq/Program.fs
@@ -0,0 +1,5 @@
+﻿let seq = 
+    Seq.initInfinite(fun index -> if index % 2 = 0 then 1 else -1)
+
+let newSeq =
+    Seq.map(fun (x, y) -> x * y) (Seq.zip seq (Seq.initInfinite(fun index -> index + 1)))

f3fe27cfd29099bc1fd8d30704fd35960639d165
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2016-04-06 22:00:54 +0300

    map for tree

---
+++ Semester4/4/Tree/Tree/Program.fs
@@ -0,0 +1,12 @@
+﻿type Tree<'a> =
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    | Tip of 'a
+
+let rec map func tree =
+    match tree with
+    | Tip(value) -> Tip(func(value))
+    | Tree(value, left, right) -> Tree(func(value), map func left, map func right)
+
+let func = (*) 3
+
+let tree = Tree(5, Tree(4, Tip(3), Tree(6, Tip(7), Tip(8))), Tip(10))

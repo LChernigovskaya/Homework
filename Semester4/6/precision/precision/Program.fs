f08b6583b673bae5928aa9dcd2628871c6f66d3c
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2016-05-18 13:46:24 +0300

    rounded calculation

---
+++ Semester4/6/precision/precision/Program.fs
@@ -0,0 +1,11 @@
+﻿type RoundingBuilder(digit : int) =
+    member this.Bind(x : float, f : float -> float) =
+        System.Math.Round(f x, digit)
+    member this.Return(x) = x
+
+let rounding digit = new RoundingBuilder(digit)
+rounding 3 {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }

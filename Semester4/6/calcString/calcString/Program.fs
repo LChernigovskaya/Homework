08cf34844bc9bffbc61d1dd4b42d2668a3541f64
Lidiya Chernigovskaya <lidiya.chernigovskaya@gmail.com>
2016-05-18 14:24:47 +0300

    calculate strings

---
+++ Semester4/6/calcString/calcString/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+
+let strToInt str f = 
+    match Int32.TryParse(str) with
+    | false, _ -> None
+    | true, value  -> f value
+
+type MaybeBuilder() =
+    member this.Bind(m, f) = strToInt m f
+    member this.Return(x) = Some(x)
+
+let maybe = new MaybeBuilder()
+    
+let result = maybe {
+        let! x = "1"
+        let! y = "2"
+        let z = x + y
+        return z
+    }
+
+let res = maybe {
+        let! x = "1"
+        let! y = "Ъ"
+        let z = x + y
+        return z
+    }
